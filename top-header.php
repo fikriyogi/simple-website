5cdd8bb33361774576b7fc95f1a69858d2f20b72
fikriyogi <fikriyogi@gmail.com>
2015-11-13 23:18:43 +0700

    top-header.php

---
+++ top-header.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<!-- saved from url=(0035)http://plugins.adchsm.me/slidebars/ -->
+<html lang="en" class="sb-init"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<title>Slidebars</title>
+		<meta name="description" content="Slidebars is a jQuery plugin for quickly and easily implementing app style off-canvas menus and sidebars into your website.">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		
+		<!-- Stylesheets -->
+		<link href="css/css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/slidebars.min.css">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/material.min.css">
+		
+		<!-- Shims -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+		<![endif]-->
+		
+		
+		
+		<!-- Twitter Card -->
+		<meta name="twitter:card" content="summary">
+		<meta name="twitter:creator" content="@adchsm">
+		<meta property="og:url" content="http://plugins.adchsm.me/slidebars/">
+		<meta property="og:title" content="Slidebars">
+		<meta property="og:description" content="Slidebars is a jQuery plugin for quickly and easily implementing app-style revealing menus and sidebars into your website">
+		<meta property="og:image" content="http://plugins.adchsm.me/slidebars/images/twitter-card@2x.png">
+		
+		<!-- Web App -->
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+		<!-- ionicframwork -->
+		<link type="text/css" rel="stylesheet" href="css/site-ionic n.css"/>
+
+		<!-- syntaxhighlighter -->
+		<script type="text/javascript" src="js/syntaxhighlighter/shCore.js"></script>
+		<script type="text/javascript" src="js/syntaxhighlighter/shBrushPhp.js"></script>
+		<script type="text/javascript" src="js/syntaxhighlighter/shBrushJScript.js"></script>
+		<script type="text/javascript" src="js/syntaxhighlighter/shBrushSql.js"></script>
+		<script type="text/javascript" src="js/syntaxhighlighter/shBrushXml.js"></script>
+		<script type="text/javascript" src="js/syntaxhighlighter/shBrushCss.js"></script>
+
+
+		<link type="text/css" rel="stylesheet" href="css/syntaxhighlighter/shCoreEmacs.css"/>
+		<script type="text/javascript">SyntaxHighlighter.all();</script>
+		
+		<!-- Favicons -->
+		<link rel="icon" type="image/png" href="http://plugins.adchsm.me/slidebars/images/icons/16.png">
+		<link rel="icon" type="image/png" href="http://plugins.adchsm.me/slidebars/images/icons/32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="http://plugins.adchsm.me/slidebars/images/icons/48.png" sizes="48x48">
+		<link rel="icon" type="image/png" href="http://plugins.adchsm.me/slidebars/images/icons/64.png" sizes="64x64">
+		
+		<!-- Apple Touch Icons -->
+		<link rel="apple-touch-icon" href="http://plugins.adchsm.me/slidebars/images/icons/152.png" sizes="120x120">
+		<link rel="apple-touch-icon" href="http://plugins.adchsm.me/slidebars/images/icons/120.png" sizes="152x152">
+		<link rel="apple-touch-icon" href="http://plugins.adchsm.me/slidebars/images/icons/76.png" sizes="76x76">
+		<link rel="apple-touch-icon" href="http://plugins.adchsm.me/slidebars/images/icons/114.png" sizes="114x114">
+		<link rel="apple-touch-icon" href="http://plugins.adchsm.me/slidebars/images/icons/57.png" sizes="57x57">
+		<link rel="apple-touch-icon" href="http://plugins.adchsm.me/slidebars/images/icons/144.png" sizes="144x144">
+		<link rel="apple-touch-icon" href="http://plugins.adchsm.me/slidebars/images/icons/72.png" sizes="72x72">
+			
+		<!-- Startup Images -->
+		<!-- iPhone 5, 5S, 5C -->
+		<link href="http://plugins.adchsm.me/slidebars/images/startup/1096.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
+		
+		<!-- iPhone 4, 4S -->
+		<link href="http://plugins.adchsm.me/slidebars/images/startup/920.png" media="(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
+		
+		<!-- iPhone, 3G, 3GS -->
+		<link href="http://plugins.adchsm.me/slidebars/images/startup/460.png" media="(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
+		
+		<!-- iPad Retina, Mini Retina Portrait -->
+		<link href="http://plugins.adchsm.me/slidebars/images/startup/2008.png" media="(device-width: 768px) and (device-height: 1024px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
+		
+		<!-- iPad Retina, Mini Retina Landscape -->
+		<link href="http://plugins.adchsm.me/slidebars/images/startup/1496.png" media="(device-width: 768px) and (device-height: 1024px) and (orientation: landscape) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
+		
+		<!-- iPad, Mini Portrait -->
+		<link href="http://plugins.adchsm.me/slidebars/images/startup/1004.png" media="(device-width: 768px) and (device-height: 1024px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
+
+		<!-- iPad, Mini Landscape -->
+		<link href="http://plugins.adchsm.me/slidebars/images/startup/748.png" media="(device-width: 768px) and (device-height: 1024px) and (orientation: landscape) and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
+	<link rel="stylesheet" type="text/css" href="js/prettify.css"><script type="text/javascript" charset="utf-8" async src="js/button.6619238bf476cc3999511336bc046bfa.js"></script></head>
