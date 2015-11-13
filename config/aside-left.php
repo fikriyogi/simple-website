176f7e6b6c052c1eb5a24ad2228a389bb9ea6eb3
fikriyogi <fikriyogi@gmail.com>
2015-11-13 23:32:35 +0700

    aside-left.php

---
+++ config/aside-left.php
@@ -0,0 +1,19 @@
+<nav>
+		<ul class="sb-menu">
+			<li><img src="./Slidebars_files/slidebars-logo-white@2x.png" alt="Slidebars" width="118" height="40"></li>
+			<li class="sb-open"><form action="" method="get" name="FCari" id="FCari">
+    <input align="center" name="txtcari" type="text" id="txtcari" style="width:90%;" maxlength="30" value="<?php echo @$_GET['txtcari']?>">
+  <input type="submit" name="Submit" value="Cari">
+</form></li>
+			<li class="sb-close"><a href="./Slidebars_files/Slidebars.html">Home</a></li>
+			<li class="sb-close"><a href="http://plugins.adchsm.me/slidebars/index.php#download">Download</a></li>
+			<li class="sb-close"><a href="http://plugins.adchsm.me/slidebars/usage.php">Usage</a></li>
+			<li class="sb-close"><a href="http://plugins.adchsm.me/slidebars/usage.php#api">API</a></li>
+			<li class="sb-close"><a href="http://plugins.adchsm.me/slidebars/compatibility.php">Compatibility</a></li>
+			<li class="sb-close"><a href="http://plugins.adchsm.me/slidebars/issues.php">Issues</a></li>
+			<li class="sb-close"><a href="http://plugins.adchsm.me/slidebars/contact.php">Contact</a></li>
+			<li class="sb-close"><a class="github" href="https://github.com/adchsm/Slidebars">Github</a></li>
+			<li class="sb-close"><span class="sb-open-right">About the Author</span></li>
+			<li class="sb-close"><small>Slidebars © 2014 Adam Smith</small></li>
+		</ul>
+		</nav>
