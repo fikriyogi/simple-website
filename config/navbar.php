11a2c2ad166cfce2348d3d332561b600f083da33
fikriyogi <fikriyogi@gmail.com>
2015-11-13 23:33:43 +0700

    navbar.php

---
+++ config/navbar.php
@@ -0,0 +1,32 @@
+<!-- Navbar -->
+		<nav class="navbar navbar-default navbar-fixed-top sb-slide" role="navigation">
+			<!-- Left Control -->
+			<div class="sb-toggle-left navbar-left">
+				<div class="navicon-line"></div>
+				<div class="navicon-line"></div>
+				<div class="navicon-line"></div>
+			</div><!-- /.sb-control-left -->
+			
+			<!-- Right Control -->
+			<div class="sb-toggle-right navbar-right">
+				<div class="navicon-line"></div>
+				<div class="navicon-line"></div>
+				<div class="navicon-line"></div>
+			</div><!-- /.sb-control-right -->
+			
+			<div class="container">
+				<!-- Logo -->
+				<div id="logo" class="navbar-left">
+					<a href="./Slidebars_files/Slidebars.html"><img src="./Slidebars_files/slidebars-logo@2x.png" alt="Slidebars Logo" width="118" height="40"></a>
+				</div><!-- /#logo -->
+				
+				<!-- Menu -->
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="./Slidebars_files/Slidebars.html">Home</a></li>
+					<li><a href="http://plugins.adchsm.me/slidebars/index.php#download">Download</a></li>
+					<li><a href="http://plugins.adchsm.me/slidebars/usage.php">Usage</a></li>
+					<li><a href="http://plugins.adchsm.me/slidebars/contact.php">Contact</a></li>
+					<li><a id="top-arrow" href="http://plugins.adchsm.me/slidebars/#top">^</a></li>
+				</ul>
+			</div>
+		</nav>
