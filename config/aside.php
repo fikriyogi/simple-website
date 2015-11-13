b0ec389ae31a09fbaa8e8c3424669ee519d91ba4
fikriyogi <fikriyogi@gmail.com>
2015-11-13 23:32:07 +0700

    aside.php

---
+++ config/aside.php
@@ -0,0 +1,10 @@
+<aside id="about-me">
+		<img class="img-circle img-responsive img-me" width="150" height="150" src="./Slidebars_files/me@2x.jpg" alt="Profile Picture">
+		<h3>Fikri Yogi.</h3>
+		<p>I'm a web designer and front-end developer based in Barcelona. I offer bespoke web tailoring and themes for WordPress. For more info you can:</p>
+		<ul class="list-unstyled" align="center">
+			<li class="sb-close"><a class="btn btn-social btn-facebook"><i class="fa fa-facebook"></i> Sign in with Facebook</a></li>
+			<li class="sb-close"><a href="https://twitter.com/adchsm">Follow me on Twitter</a></li>
+			<li class="sb-close"><a href="http://www.adchsm.me/mailing.php">Sign up to my mailing list</a></li>
+		</ul>
+	</aside>
