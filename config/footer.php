cfc40a13e9d0b9833923a7d08ca91240f325b897
fikriyogi <fikriyogi@gmail.com>
2015-11-13 23:33:07 +0700

    footer.php

---
+++ config/footer.php
@@ -0,0 +1,7 @@
+<footer>
+					<div class="row">
+						<div class="col-xs-12">
+							<small>Slidebars © 2013 - 2014 <a href="http://www.adchsm.me/">Adam Smith</a></small>
+						</div>
+					</div>
+				</footer>
